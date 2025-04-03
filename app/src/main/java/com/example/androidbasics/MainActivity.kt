488914b38dfd710408331ea0c8cbd02008a1aee5
Gnarll <vadimfedorovich22@mail.ru>
2025-04-03 09:38:16 +0300

    add top app bar

--- app/src/main/java/com/example/androidbasics/MainActivity.kt
+++ app/src/main/java/com/example/androidbasics/MainActivity.kt
@@ -7,6 +7,7 @@ import androidx.activity.enableEdgeToEdge
 import androidx.annotation.DrawableRes
 import androidx.annotation.StringRes
 import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
@@ -16,11 +17,14 @@ import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
 import androidx.compose.material3.Card
+import androidx.compose.material3.CenterAlignedTopAppBar
+import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
 import androidx.compose.ui.layout.ContentScale
@@ -48,7 +52,7 @@ class MainActivity : ComponentActivity() {
 
 @Composable
 fun WoofApp() {
-    Scaffold { it ->
+    Scaffold(topBar = { WoofTopAppBar() }) { it ->
         LazyColumn(contentPadding = it) {
             items(dogs) {
                 DogItem(
@@ -61,6 +65,7 @@ fun WoofApp() {
 
     }
 }
+
 
 @Composable
 fun DogItem(
@@ -113,6 +118,24 @@ fun DogInformation(
             text = stringResource(R.string.years_old, dogAge),
         )
     }
+}
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun WoofTopAppBar(modifier: Modifier = Modifier) {
+    CenterAlignedTopAppBar(modifier = modifier, title = {
+        Row(  verticalAlignment = Alignment.CenterVertically
+        ) {
+            Image(
+                painter = painterResource(R.drawable.ic_woof_logo),
+                contentDescription = null,
+                modifier = Modifier
+                    .size(dimensionResource(R.dimen.image_size))
+                    .padding(dimensionResource(R.dimen.padding_small))
+            )
+            Text(stringResource(R.string.app_top_bar_text), style = MaterialTheme.typography.displayLarge)
+        }
+    })
 }
 
 @Preview
--- readMe.md
+++ readMe.md
@@ -1,4 +1,6 @@
                                Project Features
 
 - Material design theme, generated at material-foundation.github.io/material-theme-builder/
-- Shapes, Colors, Fonts 
+- Shapes, Colors, Fonts
+- Scaffold component, top app bar
+- Insets (system bar overlaps), 
