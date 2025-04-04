4d4568ea75999a9da2d1b4d3a6283778fbed0333
Gnarll <vadimfedorovich22@mail.ru>
2025-04-04 14:41:09 +0300

    remove dynamic theme color

--- app/src/main/java/com/example/androidbasics/ui/theme/Theme.kt
+++ app/src/main/java/com/example/androidbasics/ui/theme/Theme.kt
@@ -281,7 +281,7 @@ val unspecified_scheme = ColorFamily(
 fun AndroidBasicsTheme(
     darkTheme: Boolean = isSystemInDarkTheme(),
     // Dynamic color is available on Android 12+
-    dynamicColor: Boolean = true,
+    dynamicColor: Boolean = false,
     content: @Composable() () -> Unit
 ) {
 
