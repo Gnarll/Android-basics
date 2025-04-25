1d697ccdf5e4b1737ec9a7130278ee16823562ec
Gnarll <vadim.fedorovich@ggp.local>
2025-04-25 10:51:08 +0300

    add ability to share order to other apps

--- app/src/main/java/com/example/androidbasics/CupcakeScreen.kt
+++ app/src/main/java/com/example/androidbasics/CupcakeScreen.kt
@@ -1,6 +1,8 @@
 package com.example.androidbasics
 
 
+import android.content.Context
+import android.content.Intent
 import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
@@ -138,6 +140,8 @@ fun CupcakeApp(
                 )
             }
             composable(route = CupcakeScreen.Summary.name) {
+                val context = LocalContext.current
+
                 OrderSummaryScreen(
                     orderUiState = uiState,
                     onCancelButtonClicked = {
@@ -146,12 +150,31 @@ fun CupcakeApp(
                             navController = navController
                         )
                     },
-                    onSendButtonClicked = { subject: String, summary: String -> },
+                    onSendButtonClicked = { subject: String, summary: String ->
+                        shareOrder(context, subject = subject, summary = summary)
+                    },
                     modifier = Modifier.fillMaxHeight()
                 )
             }
         }
     }
+}
+
+private fun shareOrder(context: Context, subject: String, summary: String) {
+    val intent = Intent(Intent.ACTION_SEND).apply {
+        type = "text/plain"
+        putExtra(Intent.EXTRA_SUBJECT, subject)
+        putExtra(Intent.EXTRA_TEXT, summary)
+    }
+
+    context.startActivity(
+        Intent.createChooser(
+            intent,
+            context.getString(R.string.new_cupcake_order)
+        )
+    )
+
+
 }
 
 private fun cancelOrderAndNavigateToStart(viewModel: OrderViewModel, navController: NavController) {
