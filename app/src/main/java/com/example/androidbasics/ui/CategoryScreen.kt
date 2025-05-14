3bc8a93b5b52ef861f19071228f168e541becdfb
Gnarll <vadimfedorovich22@mail.ru>
2025-05-14 16:51:04 +0300

    add category screen

--- app/src/main/java/com/example/androidbasics/ui/CategoryScreen.kt
+++ app/src/main/java/com/example/androidbasics/ui/CategoryScreen.kt
@@ -1,9 +1,30 @@
 package com.example.androidbasics.ui
 
-import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.PaddingValues
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
+import androidx.compose.material3.Card
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.res.dimensionResource
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.Dp
+import androidx.compose.ui.unit.dp
+import com.example.androidbasics.R
+import com.example.androidbasics.model.CategoriesRepository
 import com.example.androidbasics.model.Category
 import com.example.androidbasics.model.RecommendedPlace
 
@@ -14,17 +35,33 @@ fun CategoryScreen(
     onRecommendedPlaceClick: (RecommendedPlace) -> Unit,
     modifier: Modifier = Modifier
 ) {
-    Column { }
+    RecommendationsList(
+        recommendations = category.recommendedPlaces,
+        onRecommendedPlaceClick = onRecommendedPlaceClick,
+        contentPadding = dimensionResource(R.dimen.padding_small),
+        modifier = modifier
+    )
 }
 
 @Composable
 fun RecommendationsList(
     recommendations: List<RecommendedPlace>,
     onRecommendedPlaceClick: (RecommendedPlace) -> Unit,
-    modifier: Modifier = Modifier
+    modifier: Modifier = Modifier,
+    contentPadding: Dp = 0.dp
 ) {
-    LazyColumn(modifier = modifier) {
+    LazyColumn(
+        verticalArrangement = Arrangement.spacedBy(dimensionResource(R.dimen.padding_small)),
+        modifier = modifier,
+        contentPadding = PaddingValues(contentPadding),
+    ) {
+        items(items = recommendations, key = { it.name }) { item ->
+            RecommendationItem(
+                recommendedPlace = item,
+                onRecommendedPlaceClick = onRecommendedPlaceClick,
+            )
 
+        }
     }
 }
 
@@ -34,5 +71,34 @@ fun RecommendationItem(
     onRecommendedPlaceClick: (RecommendedPlace) -> Unit,
     modifier: Modifier = Modifier
 ) {
+    Card(modifier = modifier.fillMaxWidth()) {
+        Row(
+            horizontalArrangement = Arrangement.spacedBy(dimensionResource(R.dimen.padding_medium)),
+            verticalAlignment = Alignment.CenterVertically,
+            modifier = Modifier
+                .clickable(
+                    onClick = { onRecommendedPlaceClick(recommendedPlace) },
+                )
+                .padding(dimensionResource(R.dimen.padding_small)),
+        ) {
+            Image(
+                painter = painterResource(recommendedPlace.image),
+                contentDescription = stringResource(recommendedPlace.name),
+                contentScale = ContentScale.Fit,
+                modifier = Modifier.width(dimensionResource(R.dimen.medium_image_size))
+            )
+            Text(
+                text = stringResource(recommendedPlace.name),
+                style = MaterialTheme.typography.bodyLarge
+            )
+        }
+    }
+}
 
+@Preview(showBackground = true)
+@Composable
+fun CategoryScreenPreview() {
+    CategoryScreen(
+        category = CategoriesRepository.getCategories().first(),
+        onRecommendedPlaceClick = {})
 }
