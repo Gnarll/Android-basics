72c412438743af3da6304968c52a794464ad86f9
Gnarll <vadim.fedorovich@ggp.local>
2025-05-16 08:38:42 +0300

    add adaptive category screen

--- app/src/main/java/com/example/androidbasics/ui/CategoryScreen.kt
+++ app/src/main/java/com/example/androidbasics/ui/CategoryScreen.kt
@@ -1,5 +1,6 @@
 package com.example.androidbasics.ui
 
+import androidx.compose.animation.animateColorAsState
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Arrangement
@@ -10,45 +11,62 @@ import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
+import androidx.compose.foundation.rememberScrollState
+import androidx.compose.foundation.verticalScroll
 import androidx.compose.material3.Card
+import androidx.compose.material3.CardDefaults
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.res.dimensionResource
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
-import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.Dp
 import androidx.compose.ui.unit.dp
 import com.example.androidbasics.R
-import com.example.androidbasics.model.CategoriesRepository
-import com.example.androidbasics.model.Category
 import com.example.androidbasics.model.RecommendedPlace
+import com.example.androidbasics.ui.utils.CityContentType
 
 
 @Composable
 fun CategoryScreen(
-    category: Category,
-    onRecommendedPlaceClick: (RecommendedPlace) -> Unit,
+    cityUiState: CityUiState,
+    contentType: CityContentType,
+    setRecommendedPlace: (RecommendedPlace) -> Unit,
+    navigateToCategory: () -> Unit,
     modifier: Modifier = Modifier
 ) {
-    RecommendationsList(
-        recommendations = category.recommendedPlaces,
-        onRecommendedPlaceClick = onRecommendedPlaceClick,
-        contentPadding = dimensionResource(R.dimen.padding_small),
-        modifier = modifier
-    )
+    if (contentType == CityContentType.LIST_AND_DETAIL) {
+        RecommendationsListAndDetail(
+            recommendations = cityUiState.currentCategory.recommendedPlaces,
+            selectedRecommendedPlace = cityUiState.currentRecommendedPlace,
+            onRecommendedPlaceClick = setRecommendedPlace,
+            modifier = modifier.padding(dimensionResource(R.dimen.padding_small))
+        )
+    } else {
+        RecommendationsList(
+            recommendations = cityUiState.currentCategory.recommendedPlaces,
+            onRecommendedPlaceClick = { place ->
+                setRecommendedPlace(place)
+                navigateToCategory()
+            },
+            contentPadding = dimensionResource(R.dimen.padding_small),
+            modifier = modifier
+        )
+    }
 }
 
 @Composable
 fun RecommendationsList(
     recommendations: List<RecommendedPlace>,
     onRecommendedPlaceClick: (RecommendedPlace) -> Unit,
+    contentPadding: Dp = 0.dp,
+    selectedRecommendedPlace: RecommendedPlace? = null,
     modifier: Modifier = Modifier,
-    contentPadding: Dp = 0.dp
 ) {
     LazyColumn(
         verticalArrangement = Arrangement.spacedBy(dimensionResource(R.dimen.padding_small)),
@@ -59,6 +77,7 @@ fun RecommendationsList(
             RecommendationItem(
                 recommendedPlace = item,
                 onRecommendedPlaceClick = onRecommendedPlaceClick,
+                isActiveItem = selectedRecommendedPlace?.let { it == item } == true
             )
 
         }
@@ -69,16 +88,29 @@ fun RecommendationsList(
 fun RecommendationItem(
     recommendedPlace: RecommendedPlace,
     onRecommendedPlaceClick: (RecommendedPlace) -> Unit,
-    modifier: Modifier = Modifier
+    modifier: Modifier = Modifier,
+    isActiveItem: Boolean = false,
 ) {
-    Card(modifier = modifier.fillMaxWidth()) {
+    val itemContainerColorAnimated
+            by animateColorAsState(
+                targetValue =
+                    if (isActiveItem) MaterialTheme.colorScheme.primaryContainer
+                    else MaterialTheme.colorScheme.surfaceContainerHighest
+            )
+
+    Card(
+        modifier = modifier
+            .fillMaxWidth()
+            .clickable(
+                onClick = { onRecommendedPlaceClick(recommendedPlace) },
+            ),
+        colors = CardDefaults.cardColors(containerColor = itemContainerColorAnimated)
+    ) {
         Row(
             horizontalArrangement = Arrangement.spacedBy(dimensionResource(R.dimen.padding_medium)),
             verticalAlignment = Alignment.CenterVertically,
             modifier = Modifier
-                .clickable(
-                    onClick = { onRecommendedPlaceClick(recommendedPlace) },
-                )
+
                 .padding(dimensionResource(R.dimen.padding_small)),
         ) {
             Image(
@@ -95,10 +127,28 @@ fun RecommendationItem(
     }
 }
 
-@Preview(showBackground = true)
 @Composable
-fun CategoryScreenPreview() {
-    CategoryScreen(
-        category = CategoriesRepository.Defaults.category,
-        onRecommendedPlaceClick = {})
+fun RecommendationsListAndDetail(
+    recommendations: List<RecommendedPlace>,
+    selectedRecommendedPlace: RecommendedPlace,
+    onRecommendedPlaceClick: (RecommendedPlace) -> Unit,
+    modifier: Modifier = Modifier
+) {
+    Row(
+        modifier = modifier,
+        horizontalArrangement = Arrangement.spacedBy(dimensionResource(R.dimen.padding_small))
+    ) {
+        RecommendationsList(
+            recommendations = recommendations,
+            selectedRecommendedPlace = selectedRecommendedPlace,
+            onRecommendedPlaceClick = onRecommendedPlaceClick, modifier = Modifier.weight(1f)
+        )
+        RecommendedPlaceItemExtended(
+            recommendedPlace = selectedRecommendedPlace,
+            modifier = Modifier
+                .weight(1f)
+                .verticalScroll(rememberScrollState())
+        )
+
+    }
 }
