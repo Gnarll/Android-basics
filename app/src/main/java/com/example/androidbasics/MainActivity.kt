2e0f6484e219cad431af45b0fadf73c6a282cd2f
Gnarll <vadim.fedorovich@ggp.local>
2025-06-05 15:53:41 +0300

    implement UI layer

--- app/src/main/java/com/example/androidbasics/MainActivity.kt
+++ app/src/main/java/com/example/androidbasics/MainActivity.kt
@@ -8,11 +8,8 @@ import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Scaffold
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.collectAsState
-import androidx.compose.runtime.getValue
 import androidx.compose.ui.Modifier
-import androidx.lifecycle.viewmodel.compose.viewModel
-import com.example.androidbasics.ui.AmphibiansViewModel
+import com.example.androidbasics.ui.screens.AmphibiansScreen
 import com.example.androidbasics.ui.theme.AndroidBasicsTheme
 
 class MainActivity : ComponentActivity() {
@@ -24,7 +21,9 @@ class MainActivity : ComponentActivity() {
                 Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
                     AmphibiansApp(
 
-                        modifier = Modifier.padding(innerPadding)
+                        modifier = Modifier
+                            .fillMaxSize()
+                            .padding(innerPadding)
                     )
                 }
             }
@@ -33,8 +32,8 @@ class MainActivity : ComponentActivity() {
 }
 
 @Composable
-fun AmphibiansApp(modifier: Modifier = Modifier) {
-    val amphibiansViewModel: AmphibiansViewModel = viewModel(factory = AmphibiansViewModel.Factory)
-    val amphibiansUiState by amphibiansViewModel.uiState.collectAsState()
-
+fun AmphibiansApp(
+    modifier: Modifier = Modifier,
+) {
+    AmphibiansScreen(modifier = modifier)
 }
