2d85642a74dc7c9d95be1e66b85a30588a05d518
Gnarll <vadimfedorovich22@mail.ru>
2025-06-25 16:47:30 +0300

    write insert and get all items tests for itemDao

---
+++ app/src/androidTest/java/com/example/androidbasics/ItemDaoTest.kt
@@ -0,0 +1,70 @@
+package com.example.androidbasics
+
+import android.content.Context
+import androidx.room.Room
+import androidx.test.core.app.ApplicationProvider
+import androidx.test.ext.junit.runners.AndroidJUnit4
+import com.example.androidbasics.data.InventoryDatabase
+import com.example.androidbasics.data.Item
+import com.example.androidbasics.data.ItemDao
+import junit.framework.TestCase.assertEquals
+import kotlinx.coroutines.flow.first
+import kotlinx.coroutines.runBlocking
+import org.junit.After
+import org.junit.Before
+import org.junit.Test
+import org.junit.runner.RunWith
+import java.io.IOException
+
+@RunWith(AndroidJUnit4::class)
+class ItemDaoTest {
+    private lateinit var itemDao: ItemDao
+    private lateinit var inventoryDatabase: InventoryDatabase
+
+    private val item1 = Item(1, "Apple", 10.0, 21)
+    private val item2 = Item(2, "Banana", 15.0, 42)
+
+    private suspend fun addOneItemToDb() {
+        itemDao.insert(item1)
+    }
+
+    private suspend fun addTwoItemsToDb() {
+        itemDao.insert(item1)
+        itemDao.insert(item2)
+    }
+
+    @Before
+    fun initializeDb() {
+        val context: Context = ApplicationProvider.getApplicationContext()
+        inventoryDatabase =
+            Room.inMemoryDatabaseBuilder(context = context, klass = InventoryDatabase::class.java)
+                .allowMainThreadQueries()
+                .build()
+
+        itemDao = inventoryDatabase.itemDao()
+    }
+
+    @After
+    @Throws(IOException::class)
+    fun destroyDb() {
+        inventoryDatabase.close()
+    }
+
+
+    @Test
+    @Throws(Exception::class)
+    fun itemDao_InsertOneItemIntoDb() = runBlocking {
+        addOneItemToDb()
+        val items = itemDao.getAll().first()
+        assertEquals(item1, items[0])
+    }
+
+    @Test
+    @Throws(Exception::class)
+    fun itemDao_GetAllItemsFromDb() = runBlocking {
+        addTwoItemsToDb()
+        val items = itemDao.getAll().first()
+        assertEquals(item1, items[0])
+        assertEquals(item2, items[1])
+    }
+}
