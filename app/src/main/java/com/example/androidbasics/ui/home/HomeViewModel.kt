dff92a061f2219086e65b1be00c6b4b421202b8a
Gnarll <vadimfedorovich22@mail.ru>
2025-06-25 15:59:53 +0300

    show items on the HomeScreen

--- app/src/main/java/com/example/androidbasics/ui/home/HomeViewModel.kt
+++ app/src/main/java/com/example/androidbasics/ui/home/HomeViewModel.kt
@@ -17,12 +17,29 @@
 package com.example.androidbasics.ui.home
 
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
 import com.example.androidbasics.data.Item
+import com.example.androidbasics.data.ItemsRepository
+import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.SharingStarted
+import kotlinx.coroutines.flow.StateFlow
+import kotlinx.coroutines.flow.map
+import kotlinx.coroutines.flow.stateIn
 
 /**
  * ViewModel to retrieve all items in the Room database.
  */
-class HomeViewModel : ViewModel() {
+class HomeViewModel(private val itemsRepository: ItemsRepository) : ViewModel() {
+
+    private val itemsFlow: Flow<HomeUiState> =
+        itemsRepository.getAllItemsStream().map { HomeUiState(it) }
+
+    val homeUiState: StateFlow<HomeUiState> = itemsFlow.stateIn(
+        scope = viewModelScope,
+        started = SharingStarted.WhileSubscribed(TIMEOUT_MILLIS),
+        initialValue = HomeUiState()
+    )
+
     companion object {
         private const val TIMEOUT_MILLIS = 5_000L
     }
