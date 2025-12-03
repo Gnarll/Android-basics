499a6ef63868474432704ea3a4391ba54c230d80
Gnarll <vadimfedorovich22@mail.ru>
2025-12-03 15:09:23 +0300

    add compose metrics generation

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -37,6 +37,11 @@ android {
     buildFeatures {
         compose = true
     }
+
+    composeCompiler {
+        reportsDestination = layout.buildDirectory.dir("compose_compiler")
+        metricsDestination = layout.buildDirectory.dir("compose_compiler")
+    }
 }
 
 dependencies {
